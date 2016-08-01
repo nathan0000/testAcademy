af3648d26b33656d675de312a197a2c9a2b8fd5d
nathan0000 <yaoliang_w@hotmail.com>
2016-08-01 13:36:57 +1000

    Initial commit 	new file:   app.js 	new file:   index.html 	new file:   quiz.html

---
+++ app.js
@@ -0,0 +1,17 @@
+angular.module('testAcademyApp', [])
+  .controller('MainCtrl', [function() {
+  	var self = this;
+  	self.questions = [
+  	  {subject: 'Math', category: 'Geometry', difficulty: '73', question: 'What is the area of the following shadow?',questionImg: 'img-href', choiceA: '15', choiceAImg: 'imgA-href', choiceB: '16', choiceBImg: 'imgB-href', choiceC: '17', choiceCimg: 'imgC-href', choiceD: '18', choiceDImg: 'imgD-href', choiceE: '19', choiceEImg: 'imgE-href', answer: 'A', answerImg: 'imgAnswer-href', explanation: 'test for explanation', explanationImg: 'imgExpalin-href'},
+  	  {subject: 'Math', category: 'Geometry', difficulty: '53', question: 'What is the area of the following shadow?',questionImg: 'img-href', choiceA: '15', choiceAImg: 'imgA-href', choiceB: '16', choiceBImg: 'imgB-href', choiceC: '17', choiceCimg: 'imgC-href', choiceD: '18', choiceDImg: 'imgD-href', choiceE: '19', choiceEImg: 'imgE-href', answer: 'A', answerImg: 'imgAnswer-href', explanation: 'test for explanation', explanationImg: 'imgExpalin-href'},
+  	  {subject: 'Math', category: 'Geometry', difficulty: '33', question: 'What is the area of the following shadow?',questionImg: 'img-href', choiceA: '15', choiceAImg: 'imgA-href', choiceB: '16', choiceBImg: 'imgB-href', choiceC: '17', choiceCimg: 'imgC-href', choiceD: '18', choiceDImg: 'imgD-href', choiceE: '19', choiceEImg: 'imgE-href', answer: 'A', answerImg: 'imgAnswer-href', explanation: 'test for explanation', explanationImg: 'imgExpalin-href'},
+  	  {subject: 'Math', category: 'Numeracy', difficulty: '83', question: 'What is the area of the following shadow?',questionImg: 'img-href', choiceA: '15', choiceAImg: 'imgA-href', choiceB: '16', choiceBImg: 'imgB-href', choiceC: '17', choiceCimg: 'imgC-href', choiceD: '18', choiceDImg: 'imgD-href', choiceE: '19', choiceEImg: 'imgE-href', answer: 'A', answerImg: 'imgAnswer-href', explanation: 'test for explanation', explanationImg: 'imgExpalin-href'},
+  	  {subject: 'Math', category: 'Algeblar', difficulty: '43', question: 'What is the area of the following shadow?',questionImg: 'img-href', choiceA: '15', choiceAImg: 'imgA-href', choiceB: '16', choiceBImg: 'imgB-href', choiceC: '17', choiceCimg: 'imgC-href', choiceD: '18', choiceDImg: 'imgD-href', choiceE: '19', choiceEImg: 'imgE-href', answer: 'A', answerImg: 'imgAnswer-href', explanation: 'test for explanation', explanationImg: 'imgExpalin-href'}
+  	  ];
+
+  	  self.quizTemplate = 'quiz.html';
+
+  	  self.getAnswer = function(question) {
+  	  	return question.explanation;
+  	  };
+  }]);
